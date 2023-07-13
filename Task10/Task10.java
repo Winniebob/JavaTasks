4d377adc5c700114d5d51caf5a5c7096835c8ebd
Winniebob <vilksmax@gmail.com>
2023-07-13 16:39:51 +0300

    Реализуйте структуру телефонной книги с сортировкой и совмещением номеров

--- Task10/Task10.java
+++ Task10/Task10.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Task10 {
 
     public static void main(String[] args) {
-        Map<String, ArrayList<Long>> phonebook = new HashMap<String, ArrayList<Long>>();
+        Map<String, ArrayList<Long>> phonebook = new HashMap<String, ArrayList<Long>>(); // Объявляем о созданнии хэш таблицы.
         addNumber(phonebook);
         printPhoneBook(phonebook);
 
@@ -23,7 +23,7 @@ public class Task10 {
             String name = scan.next();
             System.out.println("Введите Номер телефона : ");
             long value = scan.nextLong();
-            if (phonebook.containsKey(name)) {
+            if (phonebook.containsKey(name)) { // Заполняя таблицу узнаем, есть ли уже такое имя. Если есть, добавляем в него номер в лист, если нет создаем отдельную хэш таблицу и добавляя в нее
                 phonebook.get(name).add(value);
             } else {
                 ArrayList<Long> list = new ArrayList<Long>();
@@ -34,17 +34,17 @@ public class Task10 {
         }
     }
 
-    public static void printPhoneBook(Map<String, ArrayList<Long>> phonebook) {
-        ArrayList<Map.Entry<String, ArrayList<Long>>> list = new ArrayList<>(phonebook.entrySet());
+    public static void printPhoneBook(Map<String, ArrayList<Long>> phonebook) { 
+        ArrayList<Map.Entry<String, ArrayList<Long>>> list = new ArrayList<>(phonebook.entrySet()); // создаем список с параметрами хэштаблицы, принимающей значение списка с параметрами старой хэш таблицы..
         Collections.sort(list, new Comparator<Map.Entry<String, ArrayList<Long>>>() {
             @Override
-            public int compare(Map.Entry<String, ArrayList<Long>> o1, Map.Entry<String, ArrayList<Long>> o2) {
+            public int compare(Map.Entry<String, ArrayList<Long>> o1, Map.Entry<String, ArrayList<Long>> o2) { // Сортируем в порядке убывания номеров
                 return o2.getValue().size() - o1.getValue().size();
             }
         });
 
         for (Map.Entry<String, ArrayList<Long>> phon : list) {
-            System.out.println(phon.getKey() + ":" + phon.getValue());
+            System.out.println(phon.getKey() + ":" + phon.getValue()); // проходимся по списку выводя имя и телефон в консоль
         }
     }
 
